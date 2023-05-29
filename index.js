f148a2b815b657c784654a2441a96527631fdc07
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-05-29 14:34:40 +0200

    Liga 2 Women, season 2022/23: generate endpoint

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // general prerequisites for rest api
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // import of middlewares
 const corsHeader = require("./middleware/cors-header");
 
 // match data
-const liga1men = require('./routes/liga1men');
-const liga1women = require('./routes/liga1women');
+const liga1men = require("./routes/liga1men");
+const liga1women = require("./routes/liga1women");
 const liga2men = require("./routes/liga2men");
+const liga2women = require("./routes/liga2women");
 const liga3men = require("./routes/liga3men");
 const rlnomen = require("./routes/rlnomen");
 
@@ -22,13 +23,18 @@ app.use((req, res, next) => corsHeader(req, res, next));
 app.get("/liga1men", (req, res) => liga1men(req, res));
 app.get("/liga1women", (req, res) => liga1women(req, res));
 app.get("/liga2men", (req, res) => liga2men(req, res));
+app.get("/liga2women", (req, res) => liga2women(req, res));
 app.get("/liga3men", (req, res) => liga3men(req, res));
 app.get("/rlnomen", (req, res) => rlnomen(req, res));
 
 // default endpoint (is called if none of the others is matched)
-app.all('*', (req, res) => {
-  return res.status(404).json({ error: { message: 'This endpoint does not exist.' }});
+app.all("*", (req, res) => {
+  return res
+    .status(404)
+    .json({ error: { message: "This endpoint does not exist." } });
 });
 
 // listening (for development)
-app.listen(port, () => {`Listening to requests on port ${port} ...`});
+app.listen(port, () => {
+  `Listening to requests on port ${port} ...`;
+});
