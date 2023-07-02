6d1dc28399244c04c14375e8568301ece55099a9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-07-02 11:30:15 +0200

    add route for Liga 1 Men, season 2023/24

--- routes/liga1men.js
+++ routes/liga1men.js
@@ -1,12 +1,19 @@
-const mliga1_2022_23 = require('../data/liga1men/liga1_2022-23');
+const mliga1_2022_23 = require("../data/liga1men/liga1_2022-23");
+const mliga1_2023_24 = require("../data/liga1men/liga1_2023-24");
 
 const liga1men = (req, res) => {
   const { season } = req.query;
   switch (season) {
-    case '2022-23':
+    case "2023-24":
+      return res.status(200).json(mliga1_2023_24);
+    case "2022-23":
       return res.status(200).json(mliga1_2022_23);
     default:
-      return res.status(400).json({ error: { message: 'A valid season parameter has to be provided.'}});
+      return res
+        .status(400)
+        .json({
+          error: { message: "A valid season parameter has to be provided." }
+        });
   }
 };
 
