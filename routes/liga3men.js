1db4c7e85645c8e1f29871091219b4956afadcf5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-08-05 11:12:55 +0200

    send new Liga 3 Men season via api

--- routes/liga3men.js
+++ routes/liga3men.js
@@ -1,12 +1,19 @@
 const liga3_2022_23 = require("../data/liga3men/liga3_2022-23");
+const liga3_2023_24 = require("../data/liga3men/liga3_2023-24");
 
 const liga3men = (req, res) => {
   const { season } = req.query;
   switch (season) {
     case "2022-23":
       return res.status(200).json(liga3_2022_23);
+    case "2023-24":
+      return res.status(200).json(liga3_2023_24);
     default:
-      return res.status(400).json({ error: { message: 'A valid season parameter has to be provided.'}});
+      return res
+        .status(400)
+        .json({
+          error: { message: "A valid season parameter has to be provided." }
+        });
   }
 };
 
