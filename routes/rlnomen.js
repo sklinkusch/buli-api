1cf8fbe78cec40a6416bcb773c651209364b4fb7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-08-06 15:13:02 +0200

    send new rlno season data through api

--- routes/rlnomen.js
+++ routes/rlnomen.js
@@ -1,12 +1,19 @@
 const rlno_2022_23 = require("../data/rlnomen/rlno_2022-23");
+const rlno_2023_24 = require("../data/rlnomen/rlno_2023-24");
 
 const rlnomen = (req, res) => {
   const { season } = req.query;
   switch (season) {
     case "2022-23":
       return res.status(200).json(rlno_2022_23);
+    case "2023-24":
+      return res.status(200).json(rlno_2023_24);
     default:
-      return res.status(400).json({ error: { message: 'A valid season parameter has to be provided.'}});
+      return res
+        .status(400)
+        .json({
+          error: { message: "A valid season parameter has to be provided." }
+        });
   }
 };
 
