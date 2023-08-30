66088c00f0bd1b75d978b95127be7fa61b17248f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-08-30 16:38:56 +0200

    Liga 2 Women, season 2023/24: send data through api

--- routes/liga2women.js
+++ routes/liga2women.js
@@ -1,16 +1,17 @@
 const liga2_2022_23 = require("../data/liga2women/liga2_2022-23");
+const liga2_2023_24 = require("../data/liga2women/liga2_2023-24");
 
 const liga2women = (req, res) => {
   const { season } = req.query;
   switch (season) {
+    case "2023-24":
+      return res.status(200).json(liga2_2023_24);
     case "2022-23":
       return res.status(200).json(liga2_2022_23);
     default:
-      return res
-        .status(400)
-        .json({
-          error: { message: "A valid season parameter has to be provided." }
-        });
+      return res.status(400).json({
+        error: { message: "A valid season parameter has to be provided." }
+      });
   }
 };
 
