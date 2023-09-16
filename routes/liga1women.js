919fb40eb31fdddce72502cef794b14f0ca95f04
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-09-16 09:43:01 +0200

    Liga 1 Women, season 2023/24: send data through api

--- routes/liga1women.js
+++ routes/liga1women.js
@@ -1,12 +1,19 @@
-const wliga1_2022_23 = require('../data/liga1women/liga1_2022-23');
+const wliga1_2022_23 = require("../data/liga1women/liga1_2022-23");
+const wliga1_2023_24 = require("../data/liga1women/liga1_2023-24");
 
 const liga1women = (req, res) => {
   const { season } = req.query;
   switch (season) {
-    case '2022-23':
+    case "2023-24":
+      return res.status(200).json(wliga1_2023_24);
+    case "2022-23":
       return res.status(200).json(wliga1_2022_23);
     default:
-      return res.status(400).json({ error: { message: 'A valid season parameter has to be provided.' }});
+      return res
+        .status(400)
+        .json({
+          error: { message: "A valid season parameter has to be provided." }
+        });
   }
 };
 
